635d6b8bbf052d11e0937db1121a0fad6f73de8c
kritika <kritika@gmail.com>
2019-12-12 11:29:46 +0530

    commited by priya for registration

--- src/main/java/com/matrimony/cassini/controller/UserController.java
+++ src/main/java/com/matrimony/cassini/controller/UserController.java
@@ -1,12 +1,29 @@
 package com.matrimony.cassini.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matrimony.cassini.entity.User;
+import com.matrimony.cassini.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 @CrossOrigin
 public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<String> saveUser(@RequestBody User user){
+		String result =userService.saveUser(user);
+		return new ResponseEntity<>(result,HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/matrimony/cassini/service/UserServiceImpl.java
+++ src/main/java/com/matrimony/cassini/service/UserServiceImpl.java
@@ -1,8 +1,22 @@
 package com.matrimony.cassini.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.matrimony.cassini.constants.Constant;
+import com.matrimony.cassini.entity.User;
+import com.matrimony.cassini.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public String saveUser(User user) {
+	 userRepository.save(user);
+	 return Constant.REGISTRATION_SUCCESSFUL;
+	}
 
 }
